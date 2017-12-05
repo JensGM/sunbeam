f0eb0eba1335de0f0c61aa045c24c6052b13b869
Jens Gåsemyr Magnus <jegm@statoil.com>
2017-12-05 16:38:36 +0100

    Bump version to 0.0.3

--- python/sunbeam/__init__.py
+++ python/sunbeam/__init__.py
@@ -7,5 +7,5 @@ from .libsunbeam import action
 from .config     import EclipseConfig
 from .parser     import parse_deck, parse
 
-__version__     = '0.0.1'
+__version__     = '0.0.3'
 __license__     = 'GNU General Public License version 3'
